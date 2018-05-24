6cfc0a0d9fe52df721c6029b06f226faa666aaac
spion06 <spion06@users.noreply.github.com>
2018-05-24 00:46:12 -0500

    Update parse_tab_sheet.rb

--- parse_tab_sheet.rb
+++ parse_tab_sheet.rb
@@ -8,6 +8,7 @@ key_mapping = %w(z x c v b n m a s d f g h j k l q w e r t y u i o p 1 2 3 4 5 6
 def scale_to_offset(key)
   if ARGV[1]
     @scale_mapping ||= YAML.load_file(ARGV[1])
+    raise "No mapping found for #{key} in #{ARGV[1]}" unless @scale_mapping.keys.include?(key)
     @scale_mapping[key].to_i
   else
     key.to_i
@@ -16,7 +17,7 @@ end
 
 all_segments = []
 segments.each do |segment|
-        lines = segment.split("\n").map{|l| { scale: scale_to_offset(l.split('|')[0]), tabs: l.match(/\|(.*)\|/)[1] }}
+  lines = segment.split("\n").map{|l| { scale: scale_to_offset(l.split('|')[0]), tabs: l.match(/\|(.*)\|/)[1] }}
   if lines.map{|l| l[:tabs].length }.uniq.count > 1
     raise "error parsing segment. characters per segment
     #{lines.map{|l| l[:tabs].length }.inspect} #{segment}"
@@ -26,7 +27,6 @@ segments.each do |segment|
   vert_segments.times.each do |vs_idx|
     note_mapping = []
     lines.each_with_index do |line,l_idx|
-      #offset = full_scale.index(line[:scale])
       offset = line[:scale]
       matchdata = line[:tabs][vs_idx].match(/([0-9])/)
       if matchdata
